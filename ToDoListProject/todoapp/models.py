8665fd05843308220de3694eba4f423602b812e3
SARBAJIT PAUL BAPPY <116333874+SarbajitPbappy@users.noreply.github.com>
2023-09-02 18:55:02 +0600

    Delete ToDoListProject directory

--- ToDoListProject/todoapp/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class TaskModel(models.Model):
-    taskTitle = models.CharField(max_length=100)
-    taskDescription = models.TextField()
-    is_completed = models.BooleanField(default=False)
-
-    def __str__(self):
-        return self.taskTitle
